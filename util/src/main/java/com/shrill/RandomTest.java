305f731f051c502f176b5e53ef8136d56484ea0d
gspark <zgchen_chibi@163.com>
2020-04-27 20:56:22 +0800

    * 修改测试用例

--- util/src/main/java/com/shrill/RandomTest.java
+++ util/src/main/java/com/shrill/RandomTest.java
@@ -46,6 +46,15 @@ public class RandomTest {
             System.out.print(r3.nextInt(10) + " ");
         }
 
+        // 案例4
+        // 在没带参数构造函数生成的Random对象的种子缺省是当前系统时间的毫秒数。
+        Random r4 = new Random();
+        System.out.println();
+        System.out.println("使用种子缺省是当前系统时间的毫秒数的Random对象生成[0,10)内随机整数序列");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(r4.nextInt(10) + " ");
+        }
+
         RandomTest randomTest = new RandomTest();
         System.out.println();
         System.out.println("使用种子缺省是当前系统时间的毫秒数的 RandomTest 成员变量Random对象生成[0,10)内随机整数序列, 第一次循环");
